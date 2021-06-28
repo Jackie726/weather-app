ac78df941b4d548f217d7be6ff3f5f37d3d76991
Jackie726 <81977507+Jackie726@users.noreply.github.com>
2021-06-27 21:16:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -82,3 +82,5 @@ function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
+
+search("Philadelphia");
