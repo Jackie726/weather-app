406f999931429b0841ec20651b3436356788316e
Jackie726 <81977507+Jackie726@users.noreply.github.com>
2021-06-27 20:49:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ let h4 = document.querySelector("h4");
 
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = "0${hours}";
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = "0${minutes}";
+}
 let year = now.getFullYear();
 
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
