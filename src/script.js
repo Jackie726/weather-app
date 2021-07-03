cb2fffa76039c1fd129c5e3ab924088d7c1e498f
Jackie726 <81977507+Jackie726@users.noreply.github.com>
2021-07-03 17:56:44 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -30,11 +30,6 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function getForecast(coordinates) {
-  let apiKey = "51464853d37e6a5193246383448ff81f";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
 function displayForecast(response) {
   let forecast = response.data.daily;
 
