54b58b21dbf015d730a5ce24cd343256b8254bd7
Jackie726 <81977507+Jackie726@users.noreply.github.com>
2021-07-03 17:40:14 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -77,6 +77,7 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -123,8 +124,8 @@ function displayFahrenteit(event) {
   let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
-  document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemp);
   tempElement.innerHTML = Math.round(fahrenheitTemp);
+  document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemp);
 }
 
 function displayCelsius(event) {
